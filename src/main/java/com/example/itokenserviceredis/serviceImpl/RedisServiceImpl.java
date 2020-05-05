9230b51f7ab6e3713a61228e135c45c4b413aa3f
yls <1329926763@qq.com>
2020-05-05 21:43:41 +0800

    添加readme.md

--- src/main/java/com/example/itokenserviceredis/serviceImpl/RedisServiceImpl.java
+++ src/main/java/com/example/itokenserviceredis/serviceImpl/RedisServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.itokenserviceredis.serviceImpl;
 import com.example.itokenserviceredis.service.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
